b95c1faa3f6dbee9e6a9001a073708b5a3da1fbf
josephburgess <josephburgess@gmail.com>
2025-02-24 19:30:26 -0300

    stop storing settings in localstorage

--- src/context/settings.js
+++ src/context/settings.js
@@ -1,7 +1,6 @@
 import { createContext, useContext, useEffect, useState } from "react"
 import defaultConfig from "data/settings"
 
-const SETTINGS_KEY = "settings"
 const IS_DOCKER = process.env.BUILD_MODE === "docker"
 
 export const SettingsContext = createContext({
@@ -13,77 +12,31 @@ export const useSettings = () => useContext(SettingsContext)
 
 export const SettingsProvider = ({ children }) => {
 	const [settings, setSettings] = useState()
-	const [items, setItems] = useState([])
 
-	// Load settings
 	useEffect(() => {
-		let data
-
 		if (IS_DOCKER) {
 			fetch("/api/loadSettings")
 				.then((response) => response.json())
 				.then((data) => setSettings(data))
 				.catch(() => setSettings(defaultConfig))
 		} else {
-			data = localStorage.getItem(SETTINGS_KEY)
-			if (data === "undefined") {
-				console.log("LocalStorage configuration reset to defaults.")
-			}
-			setSettings(data ? JSON.parse(data) : defaultConfig)
+			fetch("/api/loadSettings")
+				.then((response) => response.json())
+				.then((data) => setSettings(data))
+				.catch(() => setSettings(defaultConfig))
 		}
 	}, [])
 
-	// Save settings
-	useEffect(() => {
-		if (settings && settings !== "undefined") {
-			if (IS_DOCKER) {
-				fetch("/api/saveSettings", {
-					method: "POST",
-					headers: {
-						"Content-Type": "application/json"
-					},
-					body: JSON.stringify(settings)
-				})
-			} else {
-				localStorage.setItem(SETTINGS_KEY, JSON.stringify(settings))
-			}
-
-			let filterArr = [
-				"help",
-				"fetch",
-				"config",
-				"config help",
-				"config edit",
-				"config import",
-				"config theme",
-				"config reset"
-			]
-
-			fetch("/api/getTheme")
-				.then((response) => response.json())
-				.then((data) => {
-					if (!data.message) {
-						data.forEach((theme) => {
-							filterArr.push("config theme " + theme)
-						})
-					}
-				})
-				.catch((error) => console.log(`Error fetching themes: ${error.message}`))
-
-			settings.sections.list.map((section) => {
-				section.links.map((link) => {
-					{
-						filterArr.push(link.name.toLowerCase())
-					}
-				})
-			})
-			setItems(filterArr)
-		}
-	}, [settings])
-
-	// Update settings
 	const updateSettings = async (newSettings) => {
-		await setSettings(newSettings)
+		setSettings(newSettings)
+
+		if (IS_DOCKER) {
+			fetch("/api/saveSettings", {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify(newSettings)
+			})
+		}
 	}
 
 	// Reset settings
@@ -92,8 +45,7 @@ export const SettingsProvider = ({ children }) => {
 	}
 
 	return (
-		<SettingsContext.Provider
-			value={{ settings, setSettings: updateSettings, resetSettings, items }}>
+		<SettingsContext.Provider value={{ settings, setSettings: updateSettings, resetSettings }}>
 			{children}
 		</SettingsContext.Provider>
 	)
